e6074e5f9afe27fc93315b14b1f50eae00a8f9d4
Ethan Nguyen <etnguyen03@hotmail.com>
2020-11-29 21:27:16 -0500

    feat: lazy-load images

--- photomanager/static/js/main.js
+++ photomanager/static/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-   $(".img-spinner").on("load", function() {
-       $(this).parent().children(".spinner-wrapper").hide();
-   })
+    $(".img-spinner").on("load", function() {
+        $(this).parent().children(".spinner-wrapper").hide();
+    });
+
+    $('.img-spinner-lazy').lazy({
+        afterLoad: function(element) {
+            element.parent().children(".spinner-wrapper").hide();
+        }
+    });
 });
