a51a59981c8862a176fa1f78dcccd84c8b7f0557
Ethan Nguyen <etnguyen03@hotmail.com>
2020-11-30 18:25:00 -0500

    chore(vagrant): install dependencies for root user as well

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -2,6 +2,7 @@
 
 cd /home/vagrant/photomanager
 pipenv install --dev --deploy
+sudo pipenv install --dev --deploy
 
 tmux new-session -s servers "bash --init-file <(cd /home/vagrant/photomanager && sudo pipenv run celery -A photomanager worker -l INFO)" \; \
   split-window -h "bash --init-file <(cd /home/vagrant/photomanager && pipenv run ./manage.py runserver 0.0.0.0:8000)"
