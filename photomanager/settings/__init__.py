ee18bd6e21e0f222e6006c54dd940901aa2e9a71
Ethan Nguyen <etnguyen03@hotmail.com>
2020-11-29 20:03:43 -0500

    feat: redirect to Nextcloud login page (LOGIN_URL)

--- photomanager/settings/__init__.py
+++ photomanager/settings/__init__.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -134,7 +137,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "serve/")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
-LOGIN_URL = "two_factor:login"
+LOGIN_URL = reverse_lazy("social:begin", args=["nextcloud"])
 
 AUTH_USER_MODEL = "users.User"
 
