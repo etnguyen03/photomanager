31a087ca3364834e927260f63a9e709863a8d651
Ethan Nguyen <etnguyen03@hotmail.com>
2020-11-30 11:05:25 -0500

    chore(vagrant): allow running as non-superuser

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-tmux new-session -s servers "bash --init-file <(cd /home/vagrant/photomanager && pipenv run celery -A photomanager worker -l INFO)" \; \
+cd /home/vagrant/photomanager
+pipenv install --dev --deploy
+
+tmux new-session -s servers "bash --init-file <(cd /home/vagrant/photomanager && sudo pipenv run celery -A photomanager worker -l INFO)" \; \
   split-window -h "bash --init-file <(cd /home/vagrant/photomanager && pipenv run ./manage.py runserver 0.0.0.0:8000)"
