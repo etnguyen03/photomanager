ae1aa2afed808ce4a5f58f58dd6c6973669ce6b6
Ethan Nguyen <etnguyen03@hotmail.com>
2020-12-01 10:15:31 -0500

    feat(settings): add databases and cache to secret.sample.py

--- photomanager/settings/secret.sample.py
+++ photomanager/settings/secret.sample.py
@@ -12,3 +12,25 @@ SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
 SOCIAL_AUTH_NEXTCLOUD_KEY = ""
 SOCIAL_AUTH_NEXTCLOUD_SECRET = ""
 NEXTCLOUD_URI = ""  # Hostname of your Nextcloud instance, like "nextcloud.example.com"
+
+# Configure your database and cache here.
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "",  # Database name
+        "USER": "",  # Database user
+        "PASSWORD": "",  # Database password
+        "HOST": "postgres",  # Database hostname
+        "PORT": "5432",  # Database port number
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "",  # in the format redis://{{ HOSTNAME }}:{{ PORT NUMBER }}/1
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
