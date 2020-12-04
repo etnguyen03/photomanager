316404c1041dc35197f30b960238de45a66e1f8e
Ethan Nguyen <etnguyen03@hotmail.com>
2020-12-03 23:29:03 -0500

    fix(users): remove Group from Django admin
    Currently don't use it, don't need it

--- photomanager/apps/users/admin.py
+++ photomanager/apps/users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import User
 
 admin.site.register(User)
+
+# We don't need Group
+admin.site.unregister(Group)
