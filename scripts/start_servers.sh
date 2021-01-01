a5efb4e7857911c9de1c38cac40076d17f37ba8b
Ethan Nguyen <etnguyen03@hotmail.com>
2021-01-01 18:38:43 -0500

    chore: start celery worker in DEBUG mode

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -2,7 +2,7 @@
 
 cd /home/vagrant/photomanager
 
-tmux new-session -s servers "bash --init-file <(cd /home/vagrant/photomanager && sudo pipenv run celery -A photomanager worker -l INFO)" \; \
+tmux new-session -s servers "bash --init-file <(cd /home/vagrant/photomanager && sudo pipenv run celery -A photomanager worker -l DEBUG)" \; \
   split-window -h "bash --init-file <(cd /home/vagrant/photomanager && pipenv run ./manage.py runserver 0.0.0.0:8000)" \; \
   selectp -t 0 \; \
   split-window -v "bash --init-file <(cd /home/vagrant/photomanager && pipenv run celery -A photomanager beat -l DEBUG)" \; \
